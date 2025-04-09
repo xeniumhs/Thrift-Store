75f44abb13cf1de53be44da1d549c68e28d2880d
Mheteula <rushalmanandhar@gmail.com>
2025-04-09 18:03:01 +0545

    added my name

--- frontend/mern-test/src/App.jsx
+++ frontend/mern-test/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         {/* yesari nai name rakham aile afno branch ma
         then i will check merge! */}
         <p>Xenium Suwal</p>   
+        <p>Rushal Manandhar</p>
 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
