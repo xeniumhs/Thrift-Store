a2f5b591fbe1af919cd4fd98e4337eceb475ff82
xeniumhs <thexenium07@gmail.com>
2025-04-16 20:47:51 +0545

    started new project

--- backend/db/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost:27017/E-comm")
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ MongoDB Connection Failed:", err));
