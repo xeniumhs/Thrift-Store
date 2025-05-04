2e103df2208ce5c89ae6132a0f54eaefbd1be597
Mheteula <rushalmanandhar@gmail.com>
2025-05-04 16:29:20 +0545

    mfb

--- frontend/thrift-store/src/components/base/Navbar.jsx
+++ frontend/thrift-store/src/components/base/Navbar.jsx
@@ -1,33 +1,96 @@
 import React from "react";
-import './Navbar.css';
 
 export default function Navbar() {
-  const user = localStorage.getItem("user");
-
-  const logout = () => {
-    localStorage.clear();
-  };
   return (
-    <div className="Navbar">
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/upload">Upload</a>
-        </li>
-        {user ? (
-          <li>
-            <a onClick={logout} href="/login">Logout</a>
-            <a href="/profile">profile</a>
-          </li>
-        ) : (
-          <li>
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-          </li>
-        )}
-      </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="/">
+                Home
+                <span class="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  Separated link
+                </a>
+              </div>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-sm-2"
+              type="search"
+              placeholder="Search"
+            />
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">
+              Search
+            </button>
+            
+          </form>
+          {auth ?(
+            <h1>done</h1>
+          ):(
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a class="nav-link active" href="/login">Login</a>
+            </li>
+          </ul>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
