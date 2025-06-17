81a35cd45f2ca68fb13b1ed726fd9b532b5996ff
xeniumhs <thexenium07@gmail.com>
2025-06-17 12:32:48 +0545

    used db from .env instead of hard coded uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = "mongodb://localhost:27017/thrift";
+// const MONGO_URI = "mongodb://localhost:27017/thrift";
 
 // const connectDB = async () => {
 //   try {
@@ -16,7 +16,7 @@ const MONGO_URI = "mongodb://localhost:27017/thrift";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB✅");
   } catch (error) {
     console.log("Error connecting to MongoDB:❌ ", error);
